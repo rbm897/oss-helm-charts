3e516eeb77cdcc9de3d4fb62cae6117b1794b2c1
Ram Bhajan Mishra <rbm897@gmail.com>
2025-06-28 09:04:50 +0530

    updated config

--- downloadCharts.go
+++ downloadCharts.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"gopkg.in/yaml.v2"
 )
@@ -13,6 +15,7 @@ import (
 type Chart struct {
 	Name    string `yaml:"name"`
 	URL     string `yaml:"url"`
+	Chart   string `yaml:"chart"`
 	Path    string `yaml:"path"`
 	Version string `yaml:"version"`
 }
@@ -33,6 +36,40 @@ func folderExists(path string) bool {
 	return err == nil && info.IsDir()
 }
 
+func copyDir(src string, dst string) error {
+	entries, err := ioutil.ReadDir(src)
+	if err != nil {
+		return err
+	}
+	if err := os.MkdirAll(dst, 0755); err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+		if entry.IsDir() {
+			if err := copyDir(srcPath, dstPath); err != nil {
+				return err
+			}
+		} else {
+			in, err := os.Open(srcPath)
+			if err != nil {
+				return err
+			}
+			defer in.Close()
+			out, err := os.Create(dstPath)
+			if err != nil {
+				return err
+			}
+			defer out.Close()
+			if _, err := io.Copy(out, in); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
 func main() {
 	data, err := ioutil.ReadFile("charts.yaml")
 	if err != nil {
@@ -47,10 +84,16 @@ func main() {
 	addedRepos := make(map[string]bool)
 
 	for _, chart := range chartFile.Charts {
+		// Determine folder name: path if given, else chart
+		folderName := chart.Path
+		if folderName == "" {
+			folderName = chart.Chart
+		}
+
 		// Remove folder if exists
-		if folderExists(chart.Path) {
-			fmt.Printf("Removing existing folder: %s\n", chart.Path)
-			if err := os.RemoveAll(chart.Path); err != nil {
+		if folderExists(folderName) {
+			fmt.Printf("Removing existing folder: %s\n", folderName)
+			if err := os.RemoveAll(folderName); err != nil {
 				log.Fatalf("Failed to remove folder: %v", err)
 			}
 		}
@@ -64,19 +107,31 @@ func main() {
 			addedRepos[chart.Name] = true
 		}
 
-		// Update repos (optional, but good to keep charts fresh)
+		// Update repos
 		fmt.Println("Updating helm repos...")
 		if err := runCmd("helm", "repo", "update"); err != nil {
 			log.Fatalf("Failed to update repos: %v", err)
 		}
 
-		// Pull the chart
-		fmt.Printf("Pulling chart: %s/%s\n", chart.Name, chart.Path)
+		// Use a temp directory for untar
+		tmpDir, err := ioutil.TempDir("", "helm-pull")
+		if err != nil {
+			log.Fatalf("Failed to create temp dir: %v", err)
+		}
+		defer os.RemoveAll(tmpDir)
+
+		fmt.Printf("Pulling chart: %s/%s into temp dir\n", chart.Name, chart.Chart)
 		if err := runCmd("helm", "pull",
-			fmt.Sprintf("%s/%s", chart.Name, chart.Path),
+			fmt.Sprintf("%s/%s", chart.Name, chart.Chart),
 			"--version", chart.Version,
-			"--untar", "--untardir", "."); err != nil {
+			"--untar", "--untardir", tmpDir); err != nil {
 			log.Fatalf("Failed to pull chart: %v", err)
+		}
+
+		// Move contents from tmpDir/chart.Chart to folderName
+		srcDir := filepath.Join(tmpDir, chart.Chart)
+		if err := copyDir(srcDir, folderName); err != nil {
+			log.Fatalf("Failed to copy chart to destination: %v", err)
 		}
 	}
 }
